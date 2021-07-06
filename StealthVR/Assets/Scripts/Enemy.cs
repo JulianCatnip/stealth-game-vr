4d28a2868d7be9bf7e176e1c1ecd7ec2e4de02f7
Sandra <sandra.radzanowski@study.hs-duesseldorf.de>
2021-07-06 22:01:38 +0200

    Changed Hero Base

---
+++ StealthVR/Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Interactable
+{
+    public override void Interact()
+    {
+        base.Interact();
+
+        // Attack the enemy
+    }
+}
---
+++ StealthVR/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : CharacterStats
+{
+    public override void Die()
+    {
+        base.Die();
+
+        // Add ragdoll effect/ death animation
+
+        Destroy(gameObject);
+    }
+}
