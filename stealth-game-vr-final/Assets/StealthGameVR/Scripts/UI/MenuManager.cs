05c7eede4a49cd5b88fbff301b6552261bdbd9f5
Sandra <sandra.radzanowski@study.hs-duesseldorf.de>
2021-08-29 21:25:52 +0200

    Added Soundeffects

--- stealth-game-vr-final/Assets/StealthGameVR/Scripts/UI/MenuManager.cs
+++ stealth-game-vr-final/Assets/StealthGameVR/Scripts/UI/MenuManager.cs
@@ -19,7 +19,7 @@ public class MenuManager : MonoBehaviour
      * */
     public void EnterGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene("Intro");
     }
 
     /**
